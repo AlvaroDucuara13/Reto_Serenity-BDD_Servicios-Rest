8c9f9c712e2ed12b8572cb78593f6fc9eb990dea
Alvaro Ducuara <alvaro.isledier@gmail.com>
2022-03-25 18:25:44 -0500

    se ha agregado un modelo de datos Post con apoyo del sistema POJO

---
+++ src/main/java/co/com/sofka/models/postModel/PatchResponse.java
@@ -0,0 +1,19 @@
+package co.com.sofka.models.postModel;
+
+public class PatchResponse {
+	private String name;
+	private String job;
+	private String updatedAt;
+
+	public String getName(){
+		return name;
+	}
+
+	public String getJob(){
+		return job;
+	}
+
+	public String getUpdatedAt(){
+		return updatedAt;
+	}
+}
