295d5055e0f6286c489083ac74d17491657333a4
Alvaro Ducuara <alvaro.isledier@gmail.com>
2022-03-25 18:19:05 -0500

    se ha agregado un modelo de datos de una persona

---
+++ src/main/java/co/com/sofka/models/datosRandomModel/DatosModelRandom.java
@@ -0,0 +1,58 @@
+package co.com.sofka.models.datosRandomModel;
+
+public class DatosModelRandom {
+
+
+    private String PostId;
+    private String IdUser;
+    private String FullName;
+    private String Email;
+
+    private String Job;
+
+
+    public String getJob() {
+        return Job;
+    }
+
+    public void setJob(String job) {
+        Job = job;
+    }
+
+
+
+
+
+
+    public String getPostId() {
+        return PostId;
+    }
+
+    public void setPostId(String postId) {
+        PostId = postId;
+    }
+
+    public String getIdUser() {
+        return IdUser;
+    }
+
+    public void setIdUser(String idUser) {
+        IdUser = idUser;
+    }
+
+    public String getFullName() {
+        return FullName;
+    }
+
+    public void setFullName(String fullName) {
+        FullName = fullName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+}
